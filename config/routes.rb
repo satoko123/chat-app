094e58f19b99a04c600a50ff386200ff35731ce5
satoko123 <moonysweets@gmail.com>
2020-12-15 23:29:24 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # ↑↑上の記述は削除しましょう↑↑
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
